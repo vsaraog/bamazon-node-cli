12e0614bd327866210c945a956bbd7928457b35a
vsaraogi <mailvsaraogi@gmail.com>
2018-08-13 02:28:10 -0400

    Now added promises in all options so can quit gracefully when user chooses the option

--- bamazonManager.js
+++ bamazonManager.js
@@ -12,31 +12,35 @@ connection.connect(function(err) {
     getUserInput(); 
 })
 
-function getUserInput() {
+function getUserInput(inputMsg) {
+    const quitMe = "Quit";
+
     let userOptions = [
         {name: "View Products for Sale", value: getProductsForSale},
         {name: "View Low Inventory", value: getLowInventory},
         {name: "Add to Inventory", value: addToInventory},
         {name: "Add New Product", value: addNewProduct},
-        {name: "Quit"}
+        {name: quitMe}
     ];
     
     let questions = [
     {
-        message: "Choose one of the option",
+        message: (typeof inputMsg === "undefined") ? "Choose one of the following option" : inputMsg,
         type: "list",
         name: "chosenOption",
         choices: userOptions
     }];
 
     inquirer.prompt(questions).then(answer => {
-        if (answer.chosenOption == "quit") {
+        if (answer.chosenOption == quitMe) {
             quitApp();
         }
         else {
-            answer.chosenOption();
-        // VIK_TODO: Uncomment later on
-        // getUserInput();
+            answer.chosenOption().then( () => {
+                getUserInput("What would you want to do next");
+            }).catch(e => {
+                console.log(e);
+            })
         }
     });
 }
@@ -57,17 +61,14 @@ function getLowInventory() {
 }
 
 function addToInventory() {
-    let queryPromise = new Promise( (resolve, reject) => {
-        runSelectQuery("select * from products").then(resp => {
-            let items = [];
-            resp.forEach(i => {
-                items.push({ name: i.product_name, value: i.item_id });
-            })
-            resolve(items);
-        }, err => {
-            reject(err);
-        });
-    });
+    return new Promise( (resolve, reject) => {
+    let queryPromise = runQuery("select * from products", resp => {
+        let items = [];
+        resp.forEach(i => {
+            items.push({ name: i.product_name, value: i.item_id });
+        })
+        return items;
+    })
 
     queryPromise.then( items => {
         let questions = [{
@@ -86,74 +87,86 @@ function addToInventory() {
         inquirer.prompt(questions).then(answer => {
             let sql = "update products set stock_quantity = stock_quantity + ? where item_id = ?";
             let inserts = [parseInt(answer.restockUnits), parseInt(answer.restockItem)];
-            sql = mysql.format(sql, inserts);
 
-            let query = connection.query(sql,
-                (err, resp) => {
-                    if (err) throw err;
-                    // VIK_QUESTION: How do I get the value dislayed that user selected in the list?
-                    console.log(clc.green(answer.restockUnits) + " items were successfully added");
-                });
-            // console.log("***DEBUG***", query.sql);
+            runQuery(mysql.format(sql, inserts), () => {
+                       // VIK_QUESTION: How do I get the value dislayed that user selected in the list?                    
+                       let msg = clc.yellow(answer.restockUnits) + 
+                       clc.green(" items were successfully added");
+                       prettyOutput(msg);
+            }).then(resp => {
+                resolve(resp);
+            }).catch(e => {
+                reject(e);
+            })
         });
+    }).catch(e => { 
+        reject(e);
     })
-    
+})
 }
 
 function addNewProduct() {
-    let queryPromise = new Promise((resolve, reject) => {
-        let sql = "select * from departments";
-        runSelectQuery(sql).then(resp => {
-            let items = [];
-            resp.forEach(i => {
-                items.push({ name: i.department_name, value: i.department_id });
-            })
-            resolve(items);
-        }, err => {
-            reject(err);
-        });
-    });
+    return new Promise( (resolve, reject) => {
+    let queryPromise = runQuery("select * from departments", resp => {
+        let items = [];
+        resp.forEach(i => {
+            items.push({ name: i.department_name, value: i.department_id });
+        })
+        return items;
+    })
 
     queryPromise.then(items => {
-        let questions = [{
-            message: "Enter product name",
-            name: "product",
-            type: "input",
-        },
-        {
-            message: "Enter product department",
-            name: "dept",
-            type: "list",
-            choices: items
-        },
-        {
-            message: "Enter price of the product",
-            name: "price",
-            type: "input",
-            validate: validatePrice
-        },
-        {
-            message: "Enter stock quantity",
-            name: "quantity",
-            type: "input",
-            validate: validateNumber
-        }
-        ];
-
-        inquirer.prompt(questions).then(answers => {
-            // console.log(answers);
-
+        inquirer.prompt(getProductAddQuestions(items)).then(answers => {
             let sql = "insert into products(product_name, dept_id, price, stock_quantity) values(" +
                 " ?, ?, ?, ?)";
             let inserts = [answers.product, parseInt(answers.dept), parseFloat(answers.price), parseInt(answers.quantity)];
-            sql = mysql.format(sql, inserts);
-            let query = connection.query(sql,
-                (err, resp) => {
-                    if (err) throw err;
-                    console.log(clc.green(answers.quantity) + " " + clc.green(answers.product) + " were added at the price of " + clc.green(answers.price) + " each");
-                });
+   
+            runQuery(mysql.format(sql, inserts), () => {
+                let msg = clc.yellow(answers.quantity) +
+                clc.yellow(' \"' + answers.product) +
+                clc.green("\" were added at the price of ") +
+                clc.yellow(answers.price) +
+                clc.green(" each");
+
+                prettyOutput(msg);
+            }).then(resp => {
+                resolve(resp);
+            }).catch(e => {
+                reject(e);
+            })
         });
+    }).catch(e => {
+        reject(e);
     })
+})
+}
+
+function getProductAddQuestions(items) {
+    let questions = [{
+        message: "Enter product name",
+        name: "product",
+        type: "input",
+    },
+    {
+        message: "Enter product department",
+        name: "dept",
+        type: "list",
+        choices: items
+    },
+    {
+        message: "Enter price of the product",
+        name: "price",
+        type: "input",
+        validate: validatePrice
+    },
+    {
+        message: "Enter stock quantity",
+        name: "quantity",
+        type: "input",
+        validate: validateNumber
+    }
+    ];
+    return questions;
 }
 
 function runQuery(sql, callback) {
@@ -184,14 +197,13 @@ function runSelectQuery(sql) {
     })
 }
 
-function printItems(items) {
-    // items.forEach(e => {
-    //     console.log("Id:", e.item_id, 
-    //     ", Name: ", e.product_name,
-    //     ", Price: ", e.price,
-    //     ", Quantity: ", e.stock_quantity);
-    // });
+function prettyOutput(msg) {
+    console.log(clc.green("-".repeat(msg.length)));
+    console.log(msg);
+    console.log(clc.green("-".repeat(msg.length)));    
+}
 
+function printItems(items) {
     console.log(columnify(items, {
         columns: ['product_name', 'price', 'stock_quantity'],
         config: {
@@ -210,7 +222,6 @@ function printItems(items) {
         }
     }
     ));
-
 }
 
 function validateNumber(value) {
